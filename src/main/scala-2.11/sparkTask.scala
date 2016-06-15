a76e1a013058489462db5c4823d946c15eaed59c
Scott Lee <scott.lee@nucleusscientific.com>
2016-06-14 22:24:10 -0400

    fixed config

--- src/main/scala-2.11/sparkTask.scala
+++ src/main/scala-2.11/sparkTask.scala
@@ -11,9 +11,9 @@ object sparkTask {
   def main(args: Array[String]) = {
 
     val conf = new SparkConf(true)
-      .set("spark.cassandra.connection.host", "127.0.0.1")
+      .set("spark.cassandra.connection.host", "172.17.0.2")
       .setJars(Seq("/home/scott/IdeaProjects/sparkCassandra/target/scala-2.11/hello-assembly-1.0.jar"))
-    val sc = new SparkContext("spark://127.0.0.1:7077", "test", conf)
+    val sc = new SparkContext("spark://172.17.0.2:7077", "test", conf)
     val rdd = sc.cassandraTable("test", "kv")
     println(rdd.count)
     println(rdd.first)
